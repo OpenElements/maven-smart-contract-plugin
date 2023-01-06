c981a5184ccbe8cdbd7b84264b4c801c89647bc0
Hendrik Ebbers <hendrik.ebbers@web.de>
2023-01-06 13:10:12 +0100

    write abi to sources

--- src/main/java/com/openelements/maven/contract/CompileSmartContracts.java
+++ src/main/java/com/openelements/maven/contract/CompileSmartContracts.java
@@ -3,7 +3,6 @@ package com.openelements.maven.contract;
 import com.openelements.jsolidity.compiler.SolidityCompiler;
 import com.openelements.jsolidity.compiler.SolidityCompilerOutputType;
 import com.openelements.jsolidity.compiler.SolidityCompilerResult;
-import java.io.File;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
@@ -22,6 +21,16 @@ import java.util.stream.Stream;
 @Mojo(name = "compile", defaultPhase = LifecyclePhase.COMPILE)
 public class CompileSmartContracts extends AbstractMojo {
 
+	public static final String SRC_PATH = "src/main/contracts";
+
+	public static final String TARGET_PATH = "src/main/resources";
+
+	public static final String SOL_FILE_SUFFIX = ".sol";
+
+	public static final String BIN_FILE_SUFFIX = ".bin";
+
+	public static final String ABI_FILE_SUFFIX = ".abi";
+
 	@Parameter(defaultValue = "${project}", required = true, readonly = true)
 	MavenProject project;
 
@@ -29,18 +38,18 @@ public class CompileSmartContracts extends AbstractMojo {
 		try {
 			getLog().debug("Starting to compile smart contracts");
 
-			final Path contractsSourceDir = Paths.get(project.getBasedir().getAbsolutePath(), "src/main/contracts");
-			final Path contractsTargetDir = Paths.get(project.getBuild().getOutputDirectory(), "contracts");
+			final Path contractsSourceDir = Paths.get(project.getBasedir().getAbsolutePath(), SRC_PATH);
+			final Path contractsTargetDir = Paths.get(project.getBasedir().getAbsolutePath(), TARGET_PATH);
 
 			if (!contractsSourceDir.toFile().exists() || !contractsSourceDir.toFile().isDirectory()) {
-				getLog().warn("Directory src/main/contracts does not exist. Will end tasks");
+				getLog().warn("Directory " + SRC_PATH + " does not exist. Will end tasks");
 				return;
 			}
 
 			final Set<Path> contracts = new HashSet<>();
 			try (final Stream<Path> stream = Files.walk(contractsSourceDir)) {
 				stream.filter(Files::isRegularFile)
-						.filter(file -> file.toFile().getName().toLowerCase().endsWith(".sol"))
+						.filter(file -> file.toFile().getName().toLowerCase().endsWith(SOL_FILE_SUFFIX))
 						.forEach(file -> contracts.add(file));
 			}
 			getLog().debug("Found " + contracts.size() + " contracts");
@@ -51,19 +60,19 @@ public class CompileSmartContracts extends AbstractMojo {
 									contractsSourceDir.relativize(contract).getParent())
 							.map(dir -> contractsTargetDir.resolve(dir))
 							.orElse(contractsTargetDir);
+					targetDirForContract.toFile().mkdirs();
+
 					getLog().debug(
 							"Compiling " + contract.toFile().getAbsolutePath() + " to " + targetDirForContract.toFile().getAbsolutePath());
 
 					SolidityCompiler compiler = new SolidityCompiler(contractsSourceDir.toFile(), contract.toFile().getAbsolutePath(), Set.of(SolidityCompilerOutputType.values()));
 					SolidityCompilerResult result = compiler.compile();
 
-					targetDirForContract.toFile().mkdirs();
-
-					final String contractName = contract.toFile().getName().substring(0, contract.toFile().getName().length() - 4);
-					final Path binFile = Path.of(targetDirForContract.toFile().getAbsolutePath(), contractName + ".bin");
+					final String contractName = contract.toFile().getName().substring(0, contract.toFile().getName().length() - SOL_FILE_SUFFIX.length());
+					final Path binFile = Path.of(targetDirForContract.toFile().getAbsolutePath(), contractName + BIN_FILE_SUFFIX);
 					Files.writeString(binFile, result.contracts().iterator().next().bin());
 
-					final Path abiFile = Path.of(targetDirForContract.toFile().getAbsolutePath(), contractName + ".abi");
+					final Path abiFile = Path.of(targetDirForContract.toFile().getAbsolutePath(), contractName + ABI_FILE_SUFFIX);
 					Files.writeString(abiFile, result.contracts().iterator().next().rawAbi());
 
 				} catch (final Exception e) {
